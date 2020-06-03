5ca5214d362c36e2fbe6c1b03c69089dd6175cfd
prathamesh <prathmesh6464@gmail.com>
2020-06-03 12:41:17 +0530

    REFACTORED : Added logger and clinique controller instace created and called display menu.

--- src/main/java/com/bridz/clinique_management/CliniqueManagementApplication.java
+++ src/main/java/com/bridz/clinique_management/CliniqueManagementApplication.java
@@ -1,10 +1,19 @@
 package com.bridz.clinique_management;
 
+import com.bridz.clinique_management.pattern.GetInstance;
+
+import org.apache.log4j.Logger;
+
 public class CliniqueManagementApplication {
 
+	public static Logger logger = Logger.getLogger(CliniqueManagementApplication.class);
+	
 	public static void main(String[] args) {
 		
-
+		GetInstance.INSTANCE.getCliniqueControllerInstance().DisplayUserMenu();
+		
+		logger.info("Main method called");
+		
 	}
 
 }
