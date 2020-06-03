7fe784d601ad25debf44b4045912acb988b0cdc3
prathamesh <prathmesh6464@gmail.com>
2020-06-03 14:17:24 +0530

    ADDED : Get instance file to create instance using singleton pattern.

---
+++ src/main/java/com/bridz/clinique_management/pattern/GetInstance.java
@@ -0,0 +1,54 @@
+package com.bridz.clinique_management.pattern;
+
+import java.util.Scanner;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.bridz.clinique_management.model.Doctor;
+import com.bridz.clinique_management.model.Patient;
+import com.bridz.clinique_management.service.CliniqueImplementation;
+import com.bridz.clinique_management.controller.CliniqueController;
+
+public enum GetInstance {
+
+	INSTANCE;
+
+	private String urlOfJsonFile = "C:\\Users\\King\\Documents\\workspace-spring-tool-suite-4-4.6.0.RELEASE\\CliniqueManagement\\CliniqueManagementJsonFile\\CliniqueManagement.json";
+
+	public Scanner getScannerInstance() {
+		return new Scanner(System.in);
+	}
+
+	public File getFileInstance() {
+
+		return new File(this.urlOfJsonFile);
+	}
+
+	public FileInputStream getFileInputStreamInstance() throws FileNotFoundException {
+		return new FileInputStream(this.getFileInstance());
+	}
+
+	public ObjectMapper getObjectMapperInstance() {
+		return new ObjectMapper();
+	}
+
+	public Patient getPatientInstance() {
+		return new Patient();
+	}
+
+	public Doctor getDoctorInstance() {
+		return new Doctor();
+	}
+
+	public CliniqueImplementation getCliniqueInstance() {
+		return new CliniqueImplementation();
+	}
+	
+	public CliniqueController getCliniqueControllerInstance() {
+		return new CliniqueController();
+	}
+}
