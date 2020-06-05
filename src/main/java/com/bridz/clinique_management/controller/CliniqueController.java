29022b0d775fb3c5d6d78c76dba5ba31a19de434
prathamesh <prathmesh6464@gmail.com>
2020-06-05 17:51:46 +0530

    REFACTORED : Changed methods in doctor() and patient() methods.

--- src/main/java/com/bridz/clinique_management/controller/CliniqueController.java
+++ src/main/java/com/bridz/clinique_management/controller/CliniqueController.java
@@ -1,34 +1,58 @@
 package com.bridz.clinique_management.controller;
 
 import com.bridz.clinique_management.pattern.GetInstance;
-import com.bridz.clinique_management.service.CliniqueImplementation;
+import com.bridz.clinique_management.service.Clinique;
 
 import java.util.Scanner;
 
 public class CliniqueController {
 
 	Scanner scanner = GetInstance.INSTANCE.getScannerInstance();
-	CliniqueImplementation cliniqueImplementation = GetInstance.INSTANCE.getCliniqueInstance();
+	Clinique clinique = GetInstance.INSTANCE.getCliniqueInstance();
 
 	public void DisplayUserMenu() {
-		
+
 		System.out.println("\n*****************Welcome to Jio clinique******************\n");
 		System.out.println("Please enter number to click option\n");
 		System.out.println("1. Doctor");
 		System.out.println("2. Patient");
+		System.out.println("3. Doctor patient report");
+		System.out.println("4. Popular specialization");
+		System.out.println("5. Popular doctor");
 		int chosedOption = scanner.nextInt();
 
 		switch (chosedOption) {
 		case 1:
 
-			cliniqueImplementation.doctor();
+			clinique.doctor();
 			this.DisplayUserMenu();
 
 			break;
 
 		case 2:
 
-			cliniqueImplementation.patient();
+			clinique.patient();
+			this.DisplayUserMenu();
+
+			break;
+
+		case 3:
+
+			clinique.doctorPatientReport();
+			this.DisplayUserMenu();
+
+			break;
+
+		case 4:
+
+			clinique.popularSpecialization();
+			this.DisplayUserMenu();
+
+			break;
+
+		case 5:
+
+			clinique.popularDoctor();
 			this.DisplayUserMenu();
 
 			break;
