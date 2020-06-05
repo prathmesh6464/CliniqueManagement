78beed749c22c704b2e4903e6b18677496a53d0e
prathamesh <prathmesh6464@gmail.com>
2020-06-05 17:48:32 +0530

    REFACTORED : Added popular doctor, popular specilization, search doctor, serch patient, show patient information etc. methods added.

--- src/main/java/com/bridz/clinique_management/service/Clinique.java
+++ src/main/java/com/bridz/clinique_management/service/Clinique.java
@@ -2,7 +2,25 @@ package com.bridz.clinique_management.service;
 
 public interface Clinique {
 
+	public void doctor();
+	
+	public void patient();
+	
 	public void addPatient();
 	
 	public void addDoctor();
+	
+	public void showPatientInformation();
+	
+	public void serachPatient();
+	
+	public void showDoctors();
+	
+	public void searchDoctor() ;
+	
+	public void doctorPatientReport();
+	
+	public void popularSpecialization();
+	
+	public void popularDoctor();
 }
