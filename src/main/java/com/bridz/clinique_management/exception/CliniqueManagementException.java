3a0da0b99ec4f924d570b5763803f320f4c3e6ee
prathamesh <prathmesh6464@gmail.com>
2020-06-06 12:39:10 +0530

    REFACTORED : Changed file name and extended runtime exception

---
+++ src/main/java/com/bridz/clinique_management/exception/CliniqueManagementException.java
@@ -0,0 +1,17 @@
+package com.bridz.clinique_management.exception;
+
+public class CliniqueManagementException extends RuntimeException {
+	
+	private static final long serialVersionUID = -4005000293040820290L;
+
+	int errorCode;
+
+	// Constructor @param message
+	public CliniqueManagementException(int errorCode, String message) {
+
+		super(message);
+		this.errorCode = errorCode;
+
+	}
+
+}
