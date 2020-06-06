1ac7732184adc74a62ab6021b78f40c10f9a1f92
prathamesh <prathmesh6464@gmail.com>
2020-06-06 13:13:53 +0530

    ADDED : Patients interface file which contain add patient, show patient information, patient methods.

---
+++ src/main/java/com/bridz/clinique_management/service/Patients.java
@@ -0,0 +1,11 @@
+package com.bridz.clinique_management.service;
+
+public interface Patients {
+
+	public void addPatient();
+
+	public void showPatientInformation();
+
+	public void serachPatient();
+
+}
