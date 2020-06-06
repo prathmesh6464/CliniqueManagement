e4d077e5ccb196eafdd9c8a908c9fd0e1d12330b
prathamesh <prathmesh6464@gmail.com>
2020-06-06 13:20:25 +0530

    REFACTORED : Added clinique interface instance.

--- src/main/java/com/bridz/clinique_management/controller/CliniqueController.java
+++ src/main/java/com/bridz/clinique_management/controller/CliniqueController.java
@@ -2,6 +2,7 @@ package com.bridz.clinique_management.controller;
 
 import com.bridz.clinique_management.pattern.GetInstance;
 import com.bridz.clinique_management.service.Clinique;
+import com.bridz.clinique_management.exception.CliniqueManagementException;
 
 import java.util.Scanner;
 
@@ -9,17 +10,23 @@ public class CliniqueController {
 
 	Scanner scanner = GetInstance.INSTANCE.getScannerInstance();
 	Clinique clinique = GetInstance.INSTANCE.getCliniqueInstance();
+	int chosedOption;
 
 	public void DisplayUserMenu() {
 
 		System.out.println("\n*****************Welcome to Jio clinique******************\n");
 		System.out.println("Please enter number to click option\n");
-		System.out.println("1. Doctor");
-		System.out.println("2. Patient");
-		System.out.println("3. Doctor patient report");
+		System.out.println("1. Doctors");
+		System.out.println("2. Patients");
+		System.out.println("3. Doctors patient report");
 		System.out.println("4. Popular specialization");
 		System.out.println("5. Popular doctor");
-		int chosedOption = scanner.nextInt();
+		
+		try {
+			chosedOption = scanner.nextInt();
+		} catch (Exception exception) {
+			new CliniqueManagementException(500,"Entered value is not valid");
+		}
 
 		switch (chosedOption) {
 		case 1:
